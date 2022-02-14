b9adfcfc5ebe13f0a9a9a6ed4d2fec8e43e469d4
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-15 00:33:04 +0530

    Minor component changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,41 +1,15 @@
 import React from "react";
 
-import "./index.css";
+import PageRouters from "./PageRouters";
 
-const routes = [
-  {
-    id: "home",
-    name: "Home",
-    to: "/",
-  },
-  {
-    id: "aboutus",
-    name: "About Us",
-    to: "/aboutus",
-  },
-  {
-    id: "service",
-    name: "Service",
-    to: "/services",
-  },
-  {
-    id: "pricing",
-    name: "Pricing",
-    to: "/pricing",
-  },
-  {
-    id: "contactus",
-    name: "Contact Us",
-    to: "/contactus",
-  },
-];
+import routes from "../../routes";
+
+import "./index.css";
 
 export default function Navbar() {
   return (
     <div className="Box">
-      {routes.map((route) => (
-        <a>{route.name}</a>
-      ))}
+      <PageRouters routes={routes} />
     </div>
   );
 }
