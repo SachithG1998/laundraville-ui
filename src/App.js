fc81a944b8d23561b0367b0dc7cde599aec8d42a
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-15 00:10:16 +0530

    created homepage component(still trying)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
