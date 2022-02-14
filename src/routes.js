e045da3ba00a48c7cb89bb7e55758cfbcae2e0e2
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-15 00:32:49 +0530

    Contains the routes available in the app

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    id: "home",
+    name: "Home",
+    to: "/",
+  },
+  {
+    id: "about",
+    name: "About Us",
+    to: "/about",
+  },
+  {
+    id: "service",
+    name: "Service",
+    to: "/services",
+  },
+  {
+    id: "pricing",
+    name: "Pricing",
+    to: "/pricing",
+  },
+  {
+    id: "contact",
+    name: "Contact Us",
+    to: "/contact",
+  },
+];
+
+export default routes;
