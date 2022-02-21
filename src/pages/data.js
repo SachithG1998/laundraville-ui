36b4177c760cca8fe47484af301b5c4d93e623fd
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-21 12:29:19 +0530

    Constant data file to hold data required for the homepage

---
+++ src/pages/data.js
@@ -0,0 +1,6 @@
+export const introCardData = [
+  {
+    title: "Welcome to Laundraville!",
+    text: "A place where trust is more than just words. Rejuvanating your fasionable clothes and more has never been this breathtaking.",
+  },
+];
