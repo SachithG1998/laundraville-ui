4f0c0da17f106e0093e04b2481a111f1ac607308
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-21 19:59:53 +0530

    Responsive card layout added to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,45 @@ import { introCardData } from "./data";
 function Home() {
   return (
     <>
-      <div className="cover-area">
+      <div className="container-fluid cover-area">
         <img src={Cover} alt="Folded Laundry" className="img-fluid" />
         <div className="intro-card">
           <Card
-            cardTitle={introCardData[0].title}
-            cardText={introCardData[0].text}
+            cardTitle={introCardData.title}
+            cardText={introCardData.text}
+            cardButton={introCardData.button}
           />
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <div className="container-fluid test">
+        <div className="row gy-5">
+          <div className="col-12 col-md-6 col-lg-4">
+            <Card
+              cardTitle={introCardData.title}
+              cardText={introCardData.text}
+              cardButton={introCardData.button}
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <Card
+              cardTitle={introCardData.title}
+              cardText={introCardData.text}
+              cardButton={introCardData.button}
+            />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <Card
+              cardTitle={introCardData.title}
+              cardText={introCardData.text}
+              cardButton={introCardData.button}
+            />
+          </div>
         </div>
       </div>
     </>
