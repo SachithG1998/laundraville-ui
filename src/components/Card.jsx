8fb537b68e08ac009486322078cc5528b160d6ff
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-21 19:58:58 +0530

    Added a dynamic button that gets initialized if available

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Card({ cardTitle, cardText }) {
+function Card({ cardTitle, cardText, cardButton }) {
   return (
     <div className="card card-layout glassy dark rounded-corners">
       <div className="card-body">
         <h1 className="card-title">{cardTitle}</h1>
         <p className="card-text">{cardText}</p>
+        {typeof cardButton !== "undefined" ? (
+          <a href={cardButton.to} className="btn btn-primary">
+            {cardButton.name}
+          </a>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
