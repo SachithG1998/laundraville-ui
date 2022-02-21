31b9e4aad0832f03a48ef747a630194813677f32
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-21 12:31:17 +0530

    Structural changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./common/styles.css";
 
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
