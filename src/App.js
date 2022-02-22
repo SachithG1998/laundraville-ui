e00d342f82c1b012417fd604222a987eeb466973
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-22 10:59:01 +0530

    Added Routing capability to the app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./common/styles.css";
 
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+import About from "./pages/About";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
