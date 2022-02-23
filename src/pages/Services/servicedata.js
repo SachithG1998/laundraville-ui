9896ab368e99374d05db093a0e60b962d85e867e
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-02-23 07:44:29 +0530

    Added servicecarddata1 toservicedata

--- src/pages/Services/servicedata.js
+++ src/pages/Services/servicedata.js
@@ -1,9 +1,5 @@
-export const serviceCardData = {
+export const serviceCardData1 = {
     title: "Washing",
     text: "Paragraph about washing",
-    button: {
-      name: "",
-      to: "",
-    },
   };
   
