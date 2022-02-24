e0708d52f3b684d4da1f9f3282986ad2ba7398c1
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-24 21:14:55 +0530

    Change in import statement due to folder structure change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Cover from "../../assets/images/home/laundry.png";
-import Card from "../../components/Card";
+import Card from "../../components/card/Card";
 
 import "./Home.css";
 
