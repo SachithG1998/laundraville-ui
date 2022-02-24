8c15bcdc59dcde4b43e6979b1b76f7a23b1929d1
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-02-24 13:43:34 +0530

    n

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -4,7 +4,21 @@ import Card from "../../components/Card";
 
 import "./Services.css";
 
-import { serviceCardData } from "./servicedata";
+import { serviceCardData1 } from "./servicedata";
 
 function Services()
-
+{
+  return (
+      <>
+        <div className="row">
+          <div className="col-sm-6 ">
+            <Card
+              cardTitle={serviceCardData1.title}
+              cardText={serviceCardData1.text}
+            />
+          </div>
+          </div>
+          </>
+  );
+  }
+  export default Services;
