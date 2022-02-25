fe1c7070cbc1616ca08b42be6c856d922831d90c
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-02-25 11:57:50 +0530

    added next para

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -4,15 +4,17 @@ import Card from "../../components/Card";
 
 import "./Services.css";
 
-import { serviceCardData1 ,serviceCardData2 } from "./servicedata";
+import { serviceCardData1 ,serviceCardData2 ,serviceCardData3 } from "./servicedata";
 
 function Services()
 {
   return (
       <>
         <div className="container">
+
           <div className="container service1 ">
             <Card
+            cardImage={serviceCardData1.image}
               cardTitle={serviceCardData1.title}
               cardText={serviceCardData1.text}
             />
@@ -20,10 +22,20 @@ function Services()
 
           <div className="container service2  ">
             <Card
+            cardImage={serviceCardData2.image}
               cardTitle={serviceCardData2.title}
               cardText={serviceCardData2.text}
             />
           </div>
+
+          <div className="container service3  ">
+            <Card
+            cardImage={serviceCardData3.image}
+              cardTitle={serviceCardData3.title}
+              cardText={serviceCardData3.text}
+            />
+          </div>
+
           </div>
           </>
   );
