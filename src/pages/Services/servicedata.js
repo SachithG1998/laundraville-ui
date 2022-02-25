572ace7435da2b2a1322ffa9380cda457147c263
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-02-25 11:56:08 +0530

    Added image to the card

--- src/pages/Services/servicedata.js
+++ src/pages/Services/servicedata.js
@@ -1,14 +1,36 @@
+import imagewash1 from "../../assets/images/home/Washing Clothes.png";
+import imagewash2 from "../../assets/images/home/Drying Clothes.png";
+import imagewash3 from "../../assets/images/home/Ironing Clothes.png";
+
 export const serviceCardData1 = {
+  image :
+  {
+    src: imagewash1,
+    alt: "Washing",
+    
+  },
     title: "Washing",
     text: "Paragraph about washing",
   };
   
 export const serviceCardData2 = {
+  image :
+  {
+    src: imagewash2,
+    alt: "Ironing",
+    
+  },
   title: "Ironing",
   text: "Paragraph about ironing",
 };
 
 export const serviceCardData3 = {
+  image :
+  {
+    src: imagewash3,
+    alt: "Dry Cleaning",
+    
+  },
   title: "Dry Cleaning",
   text: "Paragraph about dry cleaning",
 };
