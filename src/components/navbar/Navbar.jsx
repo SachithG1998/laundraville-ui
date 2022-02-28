b1c9a4fb5db8184b6b775c27be6796949a973812
Sachith Gunasekara <sachithgunasekara@outlook.com>
2022-02-28 12:06:19 +0530

    Added Navbar collapsed scrolling functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,19 +24,21 @@ function Navbar() {
           <i class="fa-solid fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapsibleContent">
-          {pages.map((page) => {
-            return (
-              <div className="nav-item">
-                <a
-                  className="nav-link nav-link-font"
-                  href={page.to}
-                  key={page.id}
-                >
-                  {page.name}
-                </a>
-              </div>
-            );
-          })}
+          <div className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+            {pages.map((page) => {
+              return (
+                <div className="nav-item">
+                  <a
+                    className="nav-link nav-link-font"
+                    href={page.to}
+                    key={page.id}
+                  >
+                    {page.name}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </nav>
