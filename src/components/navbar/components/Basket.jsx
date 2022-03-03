a0ec6b146f30299fd307997f15471fa86f0e04e6
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-03-03 09:16:14 +0530

    Basket component created with the ability to show the amount of items in the basket

---
+++ src/components/navbar/components/Basket.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+import "./Basket.css";
+
+class Basket extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      basketItemCount: 0,
+    };
+  }
+
+  // Formats the basketItemCount to be readable
+  formatBasketItemCount() {
+    let formattedCount;
+
+    this.state.basketItemCount >= 100
+      ? (formattedCount = "99+")
+      : (formattedCount = this.state.basketItemCount);
+
+    return formattedCount;
+  }
+
+  render() {
+    return (
+      <div className="navbar-right">
+        <div className="p-4">
+          <i class="position-relative fa-solid fa-basket-shopping fa-2xl">
+            {this.state.basketItemCount > 0 ? (
+              <span
+                id="basket-badge"
+                class="position-absolute translate-middle badge rounded-pill bg-danger"
+              >
+                {this.formatBasketItemCount()}
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            ) : (
+              <></>
+            )}
+          </i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Basket;
