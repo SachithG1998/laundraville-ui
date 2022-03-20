b74c48fbea2b680f368efe07bb52bbd836532f01
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-03-20 16:11:23 +0530

    Service page

---
+++ src/components/card/ServiceCard.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div
+      className={`card card-layout glassy dark rounded-corners ${props.cardStyle}`}
+      data-aos={props.dataAos}
+      data-aos-duration={props.dataAosDuration}
+      data-aos-delay={props.dataAosDelay}
+    >
+      {typeof props.cardImage !== "undefined" ? (
+        <img
+          src={props.cardImage.src}
+          alt={props.cardImage.alt}
+          className="card-image-right"
+        />
+      ) : (
+        <></>
+      )}
+
+      <div className="card-body">
+        <h1
+          className="card-title"
+          data-aos="fade-right"
+          data-aos-duration="2000"
+        >
+          {props.cardTitle}
+        </h1>
+
+        <p
+          className="card-text"
+          data-aos="fade-right"
+          data-aos-delay="100"
+          data-aos-duration="2000"
+        >
+          {props.cardText}
+        </p>
+
+        {typeof props.cardButton !== "undefined" ? (
+          <a
+            href={props.cardButton.to}
+            className="btn btn-primary"
+            data-aos="fade-in"
+            data-aos-duration="2000"
+          >
+            {props.cardButton.name}
+          </a>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
