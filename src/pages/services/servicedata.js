0b6615b4b9a1f2dcd90665fcbf5ec2936f81b30f
Yoshitha Wickramarathne <laiy.yoshi@gmail.com>
2022-03-22 12:23:30 +0530

    servicedata para update

--- src/pages/services/servicedata.js
+++ src/pages/services/servicedata.js
@@ -1,32 +1,70 @@
-import imagewash1 from "../../assets/images/home/Washing Clothes.png";
-import imagewash2 from "../../assets/images/home/Drying Clothes.png";
-import imagewash3 from "../../assets/images/home/Ironing Clothes.png";
+import wash from "../../assets/images/services/Washing.png";
+import dry from "../../assets/images/services/drycleaning.jpeg";
+import iron from "../../assets/images/services/ironing.jpeg";
+import sew from "../../assets/images/services/sewing.jpg";
 
 const serviceCardData = [
   {
+    id: 1,
     image: {
-      src: imagewash1,
-      alt: "Washing",
+      src: wash,
+      alt: "",
+      att: (
+        <a href="https://lovepik.com/images/png-cartoon.html">
+          Cartoon Png vectors by Lovepik.com
+        </a>
+      ),
     },
-    title: "Washing",
-    text: "Paragraph about washing",
+    title: "Dirty to Clean",
+    text: "We wash your clothes for you at an affordable price. Laundraville offers a high quality and convenient washing service to all our customers. Each piece of cloth that is brought to Laundromat will be washed with great care using advanced cleaning techniques.",
+    animationDelay: baseTimeForDelay,
   },
-  {
-    image: {
-      src: imagewash2,
-      alt: "Ironing",
-    },
-    title: "Ironing",
-    text: "Paragraph about ironing",
-  },
-  {
-    image: {
-      src: imagewash3,
-      alt: "Dry Cleaning",
-    },
-    title: "Dry Cleaning",
-    text: "Paragraph about dry cleaning",
-  },
-];
 
-export default serviceCardData;
+  {
+    id: 2,
+    image: {
+    src: dry,
+    alt: "Laundry Clothes Drying",
+    att: (
+      <a href="https://lovepik.com/images/png-clothes-drying.html">
+        Clothes Drying Png vectors by Lovepik.com
+      </a>
+    ),
+  },
+  title: "Wrinkly to Straight",
+  text: "We make infinity percent sure that your laundry receive the love and care that you would show them.",
+  animationDelay: baseTimeForDelay * 2,
+},
+
+{
+  id: 3,
+  image: {
+    src: iron,
+    alt: "Woman Ironing CLothes",
+    att: (
+      <a href="https://lovepik.com/images/png-clothes-drying.html">
+        Clothes Drying Png vectors by Lovepik.com
+      </a>
+    ),
+  },
+  title: "Soaked to Dry",
+  text: "We just haven't told people yet but here's a secret...your laundry and us are seeing each other. I think we have enough chemistry to make it work.",
+  animationDelay: baseTimeForDelay * 3,
+},
+
+{
+  id: 4,
+    image: {
+      src: sew,
+      alt: "sewing CLothes",
+      att: (
+        <a href="https://lovepik.com/images/png-clothes-drying.html">
+          Clothes Drying Png vectors by Lovepik.com
+        </a>
+      ),
+    },
+    title: "Mending",
+    text: "We just haven't told people yet but here's a secret...your laundry and us are seeing each other. I think we have enough chemistry to make it work.",
+    animationDelay: baseTimeForDelay * 4,
+  },
+]
