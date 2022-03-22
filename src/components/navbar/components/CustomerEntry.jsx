7c3c27a746351975530d99c9a35a56bc1f9b2462
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-03-22 12:15:45 +0530

    Entry point for customer

---
+++ src/components/navbar/components/CustomerEntry.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CustomerEntry() {
+  return (
+    <div className="">
+      <a type="button" className="btn btn-outline-primary m-2">
+        Log In
+      </a>
+      <a type="button" href="/signup" className="btn btn-primary m-2">
+        Sign Up
+      </a>
+    </div>
+  );
+}
+
+export default CustomerEntry;
