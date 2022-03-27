dc30238e790cc043978148f728b2af3110ddfcbc
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-03-27 21:06:58 +0530

    Compinent changes

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -9,7 +9,7 @@ import "./SignUp.css";
 const axios = require("axios").default;
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_LAUNDRAVILLE_UI_API_URL}/customer`,
+  baseURL: `${process.env.REACT_APP_LAUNDRAVILLE_UI_API_URL}`,
 });
 
 function SignUp() {
@@ -94,7 +94,7 @@ function SignUp() {
 
   const registerCustomer = async () => {
     await api
-      .post("/register", customer)
+      .post("/customer/register", customer)
       .then((res) => {
         const { status, data } = res;
 
