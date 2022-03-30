52f130dfd5157fb5b4945de4bf9f46896029d35a
Hiruni Kaushalya <hiru036kaushalya@gmail.com>
2022-03-30 09:48:04 +0530

    initial commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-
+import cover from "../../../assets/images/about/cover.jpg";
+import "./about.css";
 function About() {
   return (
     <div>
-      {" "}
       <main style={{ padding: "10rem 0" }}>
-        <h2>Aboy us</h2>
+        <h2>About us</h2>
+        <p>
+          if you are residing in colombo and looking for quality dry cleaning
+          and laundry services nearby your areas, then choose our Laundry, One
+          of the best launderette company in colombo. our Laundry is proud to do
+          24 hours laundry and dry cleaning services with free same day
+          collection and next day delivery. Our services include wash, dry &
+          fold, wash and iron, dry cleaning, ironing service only, home and
+          bedding, clothing alteration and shoe repair. The service is simple –
+          Place an order in a matter of seconds, Our driver will come to your
+          address as per your collection time, pickup your clothes, bring them
+          to our partner facility then cleaning expert do processing and next
+          day deliver to your door step in your delivery schedule. Our customer
+          service employees ensure you face no difficulty. Customer’s every item
+          we clean is treated with care and respect by our highly experienced
+          team of experts who provides world class cleaning service, every time.
+          We are working hard to earn your trust and It’s our pleasure to keep
+          our customers satisfied with our quality & 100% happiness services
+        </p>
       </main>
     </div>
   );
 }
-
 export default About;
