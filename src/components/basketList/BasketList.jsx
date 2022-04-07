917018b62947a5f755fc732d5096ef00164b67da
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-04-07 09:02:19 +0530

    BasketList component

--- src/components/basketList/BasketList.jsx
+++ src/components/basketList/BasketList.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function BasketList() {
-  return <div>BasketList</div>;
+  return (
+    <div
+      class="offcanvas offcanvas-end glassy light vh-100"
+      tabindex="-1"
+      id="basketList"
+      aria-labelledby="basketListLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 id="basketListLabel">Your BasketList</h5>
+        <button
+          type="button"
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">...</div>
+    </div>
+  );
 }
 
 export default BasketList;
