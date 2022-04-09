b390c6bc6e8f02e48e43d155b920c65c08ea7863
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-04-09 12:56:11 +0530

    Fixed About page styling

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,69 +1,76 @@
 import React from "react";
 import "./about.css";
+
+import Sachith from "../../assets/images/about/sachith.png";
+import Yoshitha from "../../assets/images/about/yoshitha.png";
+import Hiruni from "../../assets/images/about/hiruni.png";
+
 function About() {
   return (
-    <div>
-      <h2>About us</h2>
-      <div className="paragraph">
-        <p className="text0">
-          if you are residing in colombo and looking for quality dry cleaning
-          and laundry services nearby your areas, then choose our Laundry, One
-          of the best launderette company in colombo. our Laundry is proud to do
-          24 hours laundry and dry cleaning services with free same day
-          collection and next day delivery. Our services include wash, dry &
-          fold, wash and iron, dry cleaning, ironing service only, home and
-          bedding, clothing alteration and shoe repair. The service is simple –
-          Place an order in a matter of seconds, Our driver will come to your
-          address as per your collection time, pickup your clothes, bring them
-          to our partner facility then cleaning expert do processing and next
-          day deliver to your door step in your delivery schedule. Our customer
-          service employees ensure you face no difficulty. Customer’s every item
-          we clean is treated with care and respect by our highly experienced
-          team of experts who provides world class cleaning service, every time.
-          We are working hard to earn your trust and It’s our pleasure to keep
-          our customers satisfied with our quality & 100% happiness services
-        </p>
+    <div className="page-about" style={{ marginTop: "100px" }}>
+      <div className="glassy light rounded-corners mx-5">
+        <div className="text-center pt-3">
+          <h2>About Us</h2>
+        </div>
+
+        <div className="paragraph">
+          <p className="text0">
+            if you are residing in colombo and looking for quality dry cleaning
+            and laundry services nearby your areas, then choose our Laundry, One
+            of the best launderette company in colombo. our Laundry is proud to
+            do 24 hours laundry and dry cleaning services with free same day
+            collection and next day delivery. Our services include wash, dry &
+            fold, wash and iron, dry cleaning, ironing service only, home and
+            bedding, clothing alteration and shoe repair. The service is simple
+            – Place an order in a matter of seconds, Our driver will come to
+            your address as per your collection time, pickup your clothes, bring
+            them to our partner facility then cleaning expert do processing and
+            next day deliver to your door step in your delivery schedule. Our
+            customer service employees ensure you face no difficulty. Customer’s
+            every item we clean is treated with care and respect by our highly
+            experienced team of experts who provides world class cleaning
+            service, every time. We are working hard to earn your trust and It’s
+            our pleasure to keep our customers satisfied with our quality & 100%
+            happiness services
+          </p>
+        </div>
       </div>
-      <div className="div2">
+      <div className="glassy light rounded-corners mx-5 my-5 py-4">
         <div className="container-fluid">
-          <h2>meet our team..</h2>
-          <p>we olways like to help you</p>
-          <div className="row">
-            <div className="col">
-              <img
-                src="../../assets/images/about/sachith.png"
-                className="img-fluid rounded-circle"
-                alt=""
-              />
-              <p className="text1">
-                sachith
-                <br>email:</br>
-                <br>tel no:+94714056005</br>
-              </p>
-            </div>
-            <div className="col">
-              <img
-                src="../../assets/images/about/yoshitha.png"
-                className="img-fluid rounded-circle"
-                alt=""
-              />
-              <p className="text2">
-                yoshitha
-                <br>email:</br>
-                <br>tel no:+94770666846</br>
-              </p>
-            </div>
-            <div className="col">
-              <img
-                src="../../assets/images/about/hiruni.png"
-                className="img-fluid rounded-circle"
-                alt=""
-              />
-              <p className="text3">
-                hiruni kaushalya
-                <br>email:</br>
-                <br>tel no:+94741090036</br>
-              </p>
+          <div className="text-center">
+            <h2>Meet our team...</h2>
+            <p>We always like to help you</p>
+            <div className="row g-5">
+              <div className="col-12 col-md-6 col-lg-4">
+                <img
+                  src={Sachith}
+                  className="img-fluid rounded-circle"
+                  alt="Sachith"
+                />
+                <div className="my-3">
+                  <h4>Sachith</h4>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4">
+                <img
+                  src={Yoshitha}
+                  className="img-fluid rounded-circle"
+                  alt="Sachith"
+                />
+                <div className="my-3">
+                  <h4>Yoshitha</h4>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 col-lg-4">
+                <img
+                  src={Hiruni}
+                  className="img-fluid rounded-circle"
+                  alt="Sachith"
+                />
+                <div className="my-3">
+                  <h4>Hiruni</h4>
+                </div>
+              </div>
             </div>
           </div>
         </div>
